2edde41de0fb9c42f867ad7a8935760835fd0f01
OneSleepyDev <trist.d.smith@gmail.com>
2018-06-16 21:22:18 -0700

    Fixed movement, made basic map/thing

--- Star.gd
+++ Star.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-const SPEED = 360
+const SPEED = 180
 
 
 func _physics_process(delta):
 	var motion = Vector2()
 	
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		motion += Vector2(0, -1)
-	if Input.is_action_pressed("move_bottom"):
+	if Input.is_action_pressed("ui_down"):
 		motion += Vector2(0, 1)
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		motion += Vector2(-1, 0)
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		motion += Vector2(1, 0)
 	
 	motion = motion.normalized() * SPEED
