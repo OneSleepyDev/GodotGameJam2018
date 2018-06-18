d4ab47e15e5006a8b422d9016ec2f5c92be701de
Daniel Chavarria <daniel.chavarria@kccd.edu>
2018-06-18 08:24:06 -0700

    added logic for amount of impact, and collision causes either +/- impact

--- Planet.gd
+++ Planet.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 # class member variables go here, for example:
-var isBad = false;
+var isBad = false
+var amountOfImpact = 0
+
+# constants
+const MAXIMUM_IMPACT = 50
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -9,8 +13,16 @@ func _ready():
 	pass
 
 func _init():
-	isBad = bool(round(rand_range(0,1)));
-	
+	isBad = bool(round(rand_range(0,1)))
+	amountOfImpact = round(rand_range(0,MAXIMUM_IMPACT))
+
+
+func _collision ():
+	if (isBad)
+		return amountOfImpact*-1
+	else:
+		return amountOfImpact
+
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
